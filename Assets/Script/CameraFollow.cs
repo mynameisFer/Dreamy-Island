bc0e27cb9dec8b3d131b46acc9f8bd8f89d0a01a
nxnxmi0-afk <nxnxmi0@gmail.com>
2025-11-21 19:17:30 +0700

    CameraFollow

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;      
+    public float smoothSpeed = 0.15f; 
+    public Vector3 offset;      
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothed = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = new Vector3(smoothed.x, smoothed.y, transform.position.z);
+    }
+}
+
